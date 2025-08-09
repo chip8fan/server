0a24c313e4a917aa4ef78c0c774256fa77f97f42
chip8fan <nguyennamtran2022@gmail.com>
2025-08-09 07:23:39 -0500

    UPDATE

--- client.py
+++ client.py
@@ -4,5 +4,8 @@ This is the messaging app client. (Work in Progress)
 import socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 	s.connect((socket.gethostbyname('centralserver.local'), 8000)) # connect to centralserver
-	data = s.recv(1024).decode("utf-8")
-	print(data)
+	while True:
+		data = s.recv(1024).decode("utf-8")
+		print(f"Them: {data}")
+		msg = input("Me: ").encode("utf-8")
+		s.sendall(msg)
--- server.py
+++ server.py
@@ -3,11 +3,19 @@ This is a basic server for messaging on the local network. (Work in Progress)
 """
 
 import socket
-clients = []
+sockets = []
+running = True
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 	s.bind(('', 8000)) # port 8000 must be opened via sudo ufw allow 8000
 	s.listen(2) # connect up to 2 clients
-	conn, addr = s.accept()
-	clients.append(addr)
-	s.sendall(b'Hello')
-print(clients)
+	for _ in range(2):
+		conn, addr = s.accept()
+		sockets.append(conn)
+	while running:
+		for sock in sockets:
+			data = sock.recv(1024)
+			if data.decode("utf-8") == "bye":
+				running = False
+			for client in sockets:
+				if client != sock:
+					client.sendall(data)
