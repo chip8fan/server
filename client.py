3b09238e190d2bf147959c78c32c6b02d605aaac
chip8fan <nguyennamtran2022@gmail.com>
2025-08-08 20:58:26 -0500

    UPDATE

--- client.py
+++ client.py
@@ -4,12 +4,5 @@ This is the messaging app client. (Work in Progress)
 import socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 	s.connect((socket.gethostbyname('centralserver.local'), 8000)) # connect to centralserver
-	msg = input("Me: ").encode("utf-8")
-	s.sendall(msg)
-	reply = b"Them: "
-	while True:
-		data = s.recv(1024)
-		if not data:
-			break
-		reply = reply + data
-	print(reply)
+	data = s.recv(1024).decode("utf-8")
+	print(data)
--- server.py
+++ server.py
@@ -3,14 +3,11 @@ This is a basic server for messaging on the local network. (Work in Progress)
 """
 
 import socket
+clients = []
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 	s.bind(('', 8000)) # port 8000 must be opened via sudo ufw allow 8000
 	s.listen(2) # connect up to 2 clients
-	conn1, addr1 = s.accept() # this is BLOCKING, waits for the 1st connection
-	conn2, addr2 = s.accept() # this is BLOCKING, waits for the 2nd connection
-	while True:
-		data = conn1.recv(1024)
-		conn2.sendall(data)
-		data = conn2.recv(1024)
-		conn1.sendall(data)
-
+	conn, addr = s.accept()
+	clients.append(addr)
+	s.sendall(b'Hello')
+print(clients)
